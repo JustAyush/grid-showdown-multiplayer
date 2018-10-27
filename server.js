cf65571db9b334d04a7dc55f34dd8bcc9c07e3b1
AyushBajra <realayush@gmail.com>
2018-10-27 16:54:09 +0545

    A one-on-one game

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 
 var express = require('express');
+
 var app = express();
-var server = app.listen(8000);
+var server = app.listen(process.env.PORT || 5000, listen);
+
+function listen() {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://' + host + ':' + port);
+}
 
 app.use(express.static('public'));
 
 var io = require('socket.io')(server);
+
+// var express = require('express');
+// var app = express();
+// var server = app.listen(8000);
+// app.use(express.static('public'));
+// var io = require('socket.io')(server);
 
 var players = [];
 var plready = 0;
